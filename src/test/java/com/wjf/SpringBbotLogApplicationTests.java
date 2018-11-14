366c7d5af23c0cb5df9e7e24307eb449d27a2cd9
weijunfeng <weijunfeng@jd.com>
2018-11-14 17:36:48 +0800

    springboot日志配置

--- src/test/java/com/wjf/SpringBbotLogApplicationTests.java
+++ src/test/java/com/wjf/SpringBbotLogApplicationTests.java
@@ -2,15 +2,24 @@ package com.wjf;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBbotLogApplicationTests {
-
+    //日志记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     public void contextLoads() {
+        //日志级别，由低到高   默认是info
+        logger.trace("这是trace日志");
+        logger.debug("这是调试日志");
+        logger.info("这是info日志");
+        logger.warn("这是警告日志");
+        logger.error("这是错误日志");
     }
 
 }
